c60b3994721113e7def776571582f08308a20171
Matt Raible <matt@raibledesigns.com>
2017-08-08 19:00:28 -0700

    Add Okta for security

--- client/src/app/shared/beer/beer.service.ts
+++ client/src/app/shared/beer/beer.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
+import { OktaAuthService } from '../okta/okta.service';
 
 @Injectable()
 export class BeerService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http, private oktaService: OktaAuthService) { }
 
   getAll(): Observable<any> {
-    return this.http.get('http://localhost:8081/good-beers')
+    const headers: Headers = new Headers();
+    if (this.oktaService.isAuthenticated()) {
+      const accessToken = this.oktaService.signIn.tokenManager.get('accessToken');
+      headers.append('Authorization', accessToken.tokenType + ' ' + accessToken.accessToken);
+    }
+    const options = new RequestOptions({ headers: headers });
+    return this.http.get('http://localhost:8080/good-beers', options)
       .map((response: Response) => response.json());
   }
 }
