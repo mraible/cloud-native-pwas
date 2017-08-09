c339158431dd669ed6e29bf700702d88bd2739ac
Josh Long <josh@joshlong.com>
2017-08-08 22:35:58 -0700

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 # TODO: make sure to copy the application-cloud.properties file into the beer-catalog-service and edge-service's src/main/resources directories.
 
+cp application-cloud.properties ./edge-service/src/main/resources/application-cloud.properties
+cp application-cloud.properties ./beer-catalog-service/src/main/resources/application-cloud.properties
+
+
 start=`pwd`
 
 cd $start && find . -iname pom.xml | xargs -I pom  mvn -DskipTests clean package -f pom
