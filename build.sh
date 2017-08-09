ac67628d09de4a87d052f25b6cd478d0950e234f
Josh Long <josh@joshlong.com>
2017-08-08 21:37:35 -0700

    deploy

--- build.sh
+++ build.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# TODO: make sure to copy the application-cloud.properties file into the beer-catalog-service and edge-service's src/main/resources directories.
 
 start=`pwd`
-echo start directory: $start
 
-# build the projects
-# cd $start && find . -iname pom.xml | xargs -I pom  mvn -DskipTests clean package -f pom
+cd $start && find . -iname pom.xml | xargs -I pom  mvn -DskipTests clean package -f pom
 cd $start/beer-catalog-service && ./gradlew bootRepackage
 
-# push to the cloud
 cf s | grep pwa-eureka || cf create-user-provided-service pwa-eureka -p '{"uri":"http://pwa-eureka.cfapps.io"}'
+
+cd $start/client
+rm -rf dist
+sed -i -e "s|http://localhost:8081|https://pwa-edge.cfapps.io|g" $start/client/src/app/shared/beer/beer.service.ts
+npm install && ng build -prod --aot
+touch dist/Staticfile
+
 cd $start
 cf push
+
+git checkout $r/client
+rm $r/client/src/app/shared/beer/beer.service.ts-e
