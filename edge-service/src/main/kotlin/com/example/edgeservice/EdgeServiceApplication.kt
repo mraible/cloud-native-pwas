01565b7035903bf11db6731242f0b990bcd3a6c2
Matt Raible <matt@raibledesigns.com>
2017-08-10 08:44:51 -0600

    Add WebSecurityConfig

--- edge-service/src/main/kotlin/com/example/edgeservice/EdgeServiceApplication.kt
+++ edge-service/src/main/kotlin/com/example/edgeservice/EdgeServiceApplication.kt
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.feign.EnableFeignClients
 import org.springframework.cloud.netflix.feign.FeignClient
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
